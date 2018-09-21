2563da6348e3b3b332cf33d803a57029e2b0aea0
Pierce Darragh <pierce.darragh@gmail.com>
2018-09-21 13:25:19 -0600

    Add string_of_exp function for simple pretty printing

--- pwZ_Worklist.ml
+++ pwZ_Worklist.ml
@@ -26,6 +26,41 @@ and mem  = {
   mutable end_ : pos;
   mutable result : exp }
 
+let string_of_exp (e : exp) : string =
+  let rec make_nice_string (e' : exp') (c : int) : string =
+    let indent (i : int) (ss : string list) : string list =
+      List.map (fun s -> (String.make i ' ') ^ s) ss
+    in
+    let join (ss : string list) : string =
+      String.concat "\n" ss
+    in
+    let indent_subexp_strings (es : exp list) (ind : int) : string =
+      match es with
+      | [] -> ""
+      | _ -> "\n" ^ (join (indent ind (List.map (fun e -> make_nice_string e ind) (List.map (fun e -> e.e) es))))
+    in
+
+    match e' with
+    | T (l, t) ->
+        "(T " ^ l ^ ")"
+    | Seq (l, []) ->
+        "(Seq " ^ l ^ ")"
+    | Seq (l, e :: es) ->
+        (let leader = "(Seq " ^ l ^ " " in
+         let ind = c + String.length leader in
+         leader ^ (make_nice_string e.e ind) ^ (indent_subexp_strings es ind) ^ ")"
+        )
+    | Alt (res) ->
+        match !res with
+        | [] ->
+            "(Alt)"
+        | e :: es ->
+            (let leader = "(Alt " in
+             let ind = c + String.length leader in
+             leader ^ (make_nice_string e.e ind) ^ (indent_subexp_strings es ind) ^ ")"
+            )
+  in make_nice_string e.e 0
+
 type zipper = exp' * mem
 
 let rec undefined = {
@@ -96,7 +131,6 @@ let derive (p : pos) ((t, i) : tok) ((e, m) : zipper) : unit =
                                            else d_u (Alt (ref [e])) m
 
   in d_u e m
-
 
 let init_zipper (e : exp) : zipper =
   let e' = Seq ("<init_zipper:Seq>", []) in
