b44491d81020a2e800d4e9d094fad3038615ee65
Pierce Darragh <pierce.darragh@gmail.com>
2018-09-21 09:47:39 -0600

    Implement simple tokenizer for arithmetic grammar

---
+++ arithTokenizer.ml
@@ -0,0 +1,49 @@
+open Char
+
+exception FailedMatch
+
+type token = string * int
+
+let getInt (s : string) : token = (s, 1)
+
+let getToken (c : char) : token =
+  match c with
+  | '(' -> ("(", 2)
+  | ')' -> (")", 3)
+  | '*' -> ("*", 4)
+  | '+' -> ("+", 5)
+  | _   -> raise FailedMatch
+
+let isInt (c : char) : bool =
+  Char.code c >= 48 && Char.code c <= 57
+
+let tokenize (str : string) : token list =
+  let acc = ref "" in
+
+  let append s n =
+    s := !s ^ (String.make 1 n)
+  in
+  let rest s =
+    String.sub s 1 ((String.length s) - 1)
+  in
+  let accumulate s xs =
+    if String.length s > 0
+    then (acc := "";
+          (getInt s) :: xs)
+    else xs
+  in
+
+  let rec iter s xs =
+    match s with
+    | "" -> List.rev (accumulate !acc xs)
+    | _ ->
+        (match s.[0] with
+         | ' ' ->
+             iter (rest s) xs
+         | c when isInt c ->
+             (append acc c;
+              iter (rest s) xs)
+         | c ->
+             iter (rest s) ((getToken c) :: (accumulate !acc xs))
+        )
+  in iter str []
