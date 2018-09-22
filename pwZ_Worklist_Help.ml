65e6105aa404aa5a12020edb5c3180b9fe2103b8
Pierce Darragh <pierce.darragh@gmail.com>
2018-09-21 21:59:04 -0600

    Add `plug` implementation to both _Help modules

--- pwZ_Worklist_Help.ml
+++ pwZ_Worklist_Help.ml
@@ -1,5 +1,27 @@
 open PwZ_Worklist
 
+let concat_map (f : 'a -> 'b list) (l : 'a list) : 'b list =
+  List.concat (List.map f l)
+
+let plug ((e, c) : zipper) (p : pos) : exp list =
+  let rec up (e : exp') (m : mem) : exp list =
+    let e' = { m = m_0; e = e } in
+    m.end_ <- p;
+    m.result <- e';
+    concat_map (up' e') m.parents
+  and up' (e : exp) (c : cxt) : exp list =
+    match c with
+    | Top -> [e]
+    | SeqC (m, l, left, right) ->
+        up (Seq (l, List.append (List.rev left) (e :: right))) m
+    | AltC m ->
+        if p == m.end_
+        then match m.result.e with
+             | Alt es -> es := e :: !es; []
+             | _ -> raise FailedMatch
+        else up (Alt (ref [e])) m
+  in up e c
+
 let string_of_exp (e : exp) : string =
   let rec make_nice_string (e' : exp') (c : int) : string =
     let indent (i : int) (ss : string list) : string list =
